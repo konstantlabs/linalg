41ce548776cf23b45be03d7ab2ec69b154321df1
Alvaro <alvgaona@gmail.com>
2025-01-16 19:08:58 -0300

    chores: add benchmark test

--- tests/benchmarks.rs
+++ tests/benchmarks.rs
@@ -5,7 +5,6 @@ fn add_2x2_matrices(c: &mut Criterion) {
     let m1: Matrix<i32> = Matrix::new([[1, 2], [3, 4]]);
     let m2: Matrix<i32> = Matrix::new([[5, 6], [7, 8]]);
 
-    // FIX: cannot add two matrix references
     c.bench_function("add 2x2 matrices", |b| {
         b.iter(|| black_box(&m1) + black_box(&m2))
     });
