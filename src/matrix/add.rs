b991365b1af4ce250c15d386a071a86da78b3d94
Alvaro Gaona <alvgaona@gmail.com>
2025-01-16 19:10:52 -0300

    Set up benchmarking (#2)
    * Use criterion for benchmarking
    
    * feat: add two reference matrices
    
    * chores: add benchmark test

--- src/matrix/add.rs
+++ src/matrix/add.rs
@@ -28,6 +28,33 @@ where
     }
 }
 
+impl<T> Add for &Matrix<T>
+where
+    T: Clone + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Default,
+{
+    type Output = Matrix<T>;
+
+    fn add(self, other: &Matrix<T>) -> Self::Output {
+        assert_eq!(
+            self.rows, other.rows,
+            "Matrices must have the same number of rows"
+        );
+        assert_eq!(
+            self.cols, other.cols,
+            "Matrices must have the same number of columns"
+        );
+
+        let data: Vec<T> = self
+            .data
+            .iter()
+            .zip(&other.data)
+            .map(|(a, b)| a.clone() + b.clone())
+            .collect();
+
+        Matrix::from_vec(self.rows, self.cols, data)
+    }
+}
+
 impl<T> AddAssign for Matrix<T>
 where
     T: Clone + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + AddAssign<T>,
@@ -58,6 +85,18 @@ mod tests {
         let m1: Matrix<i32> = Matrix::new([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
         let m2: Matrix<i32> = Matrix::new([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
         let m3 = m1 + m2;
+        assert_eq!(m3.rows(), 3);
+        assert_eq!(m3.cols(), 3);
+        assert_eq!(m3[(0, 0)], 2);
+        assert_eq!(m3[(1, 1)], 6);
+        assert_eq!(m3[(2, 2)], 10);
+    }
+
+    #[test]
+    fn test_add_two_matrix_refs() {
+        let m1: Matrix<i32> = Matrix::new([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+        let m2: Matrix<i32> = Matrix::new([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
+        let m3 = &m1 + &m2;
         assert_eq!(m3.rows(), 3);
         assert_eq!(m3.cols(), 3);
         assert_eq!(m3[(0, 0)], 2);
---
+++ tests/benchmarks.rs
@@ -0,0 +1,14 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use linalg::matrix::mat::Matrix;
+
+fn add_2x2_matrices(c: &mut Criterion) {
+    let m1: Matrix<i32> = Matrix::new([[1, 2], [3, 4]]);
+    let m2: Matrix<i32> = Matrix::new([[5, 6], [7, 8]]);
+
+    c.bench_function("add 2x2 matrices", |b| {
+        b.iter(|| black_box(&m1) + black_box(&m2))
+    });
+}
+
+criterion_group!(benches, add_2x2_matrices);
+criterion_main!(benches);
