926a2cccf20dfa0b58a20d29489f2b71bf8431ca
Alvaro <alvgaona@gmail.com>
2025-01-17 20:06:54 -0300

    feat: add identity, zeros and complex numbers support

--- tests/benchmarks.rs
+++ tests/benchmarks.rs
@@ -1,5 +1,8 @@
+use std::time::Duration;
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use linalg::matrix::mat::Matrix;
+use linalg::num::{Complex32, Complex64};
 
 fn add_2x2_matrices(c: &mut Criterion) {
     let m1: Matrix<i32> = Matrix::new([[1, 2], [3, 4]]);
@@ -10,5 +13,49 @@ fn add_2x2_matrices(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, add_2x2_matrices);
+fn mul_3x3_matrices(c: &mut Criterion) {
+    let m1: Matrix<i32> = Matrix::new([[2, 2, 2], [2, 2, 2], [2, 2, 2]]);
+    let m2: Matrix<i32> = Matrix::new([[2, 2, 2], [2, 2, 2], [2, 2, 2]]);
+
+    c.bench_function("mul 3x3 matrices", |b| {
+        b.iter(|| black_box(&m1) * black_box(&m2))
+    });
+}
+
+fn mul_512x512_matrices(c: &mut Criterion) {
+    use rand::{thread_rng, Rng};
+    let mut rng = thread_rng();
+    let m1: Matrix<i32> = Matrix::new([[rng.gen_range(-100..100); 512]; 512]);
+    let m2: Matrix<i32> = Matrix::new([[rng.gen_range(-100..100); 512]; 512]);
+
+    c.bench_function("mul 512x512 matrices", |b| {
+        b.iter(|| black_box(&m1) * black_box(&m2))
+    });
+}
+
+fn mul_3x3_complex32_matrices(c: &mut Criterion) {
+    let m1: Matrix<Complex32> = Matrix::new([[Complex32::new(2.0, 0.0); 3]; 3]);
+    let m2: Matrix<Complex32> = Matrix::new([[Complex32::new(2.0, 0.0); 3]; 3]);
+
+    c.bench_function("mul 3x3 complex32 matrices", |b| {
+        b.iter(|| black_box(&m1) * black_box(&m2))
+    });
+}
+
+fn mul_3x3_complex64_matrices(c: &mut Criterion) {
+    let m1: Matrix<Complex64> = Matrix::new([[Complex64::new(2.0, 0.0); 3]; 3]);
+    let m2: Matrix<Complex64> = Matrix::new([[Complex64::new(2.0, 0.0); 3]; 3]);
+
+    c.bench_function("mul 3x3 complex64 matrices", |b| {
+        b.iter(|| black_box(&m1) * black_box(&m2))
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().measurement_time(Duration::from_secs(30));
+    // targets = add_2x2_matrices, mul_3x3_matrices, mul_3x3_complex32_matrices, mul_3x3_complex64_matrices, mul_512x512_matrices
+    targets = mul_512x512_matrices
+}
+
 criterion_main!(benches);
